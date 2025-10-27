eb47901a96291dc03c6166761258cb249805ee0f
Marvin Wei <1317223009@qq.com>
2025-10-28 03:02:25 +0800

    Rev-A

---
+++ .pio/build/pico32/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/pico32/FrameworkArduino/cbuf.cpp.o: \
+ C:/Users/13172/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ C:/Users/13172/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
---
+++ .pio/build/pico32/src/ad5940.c.d
@@ -0,0 +1,1 @@
+.pio/build/pico32/src/ad5940.c.o: src/ad5940.c include/ad5940.h
